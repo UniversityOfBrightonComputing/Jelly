e6c6bfbeae432b2a5062b87b738ce55c436f5c00
Seimour <toll.everon@gmail.com>
2013-06-24 21:47:00 +0200

    Create Global.java

---
+++ game/src/jelly/Global.java
@@ -0,0 +1,19 @@
+package jelly;
+
+/**
+ * Contains global constants
+ */
+public class Global
+{
+  public static final int W = 1280;
+	public static final int H = 720;
+	
+	public static final int SPRITE_SIZE = 64;
+	
+	public static final int SCORE_COIN = 100;
+	public static final int SCORE_BONUS = SCORE_COIN * 10;
+	
+	public static final String APP_AUTHOR = "Almas";
+	public static final String APP_VERSION = "0.56";
+	public static final String APP_TITLE = "Jelly Project " + APP_VERSION + " | Author: " + APP_AUTHOR;
+}
