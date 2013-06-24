5f07d41d49690c52aebf8e3f7e3a752cbb96c597
Seimour <toll.everon@gmail.com>
2013-06-24 21:48:47 +0200

    just added @author @version to keep the class format

--- game/src/jelly/Global.java
+++ game/src/jelly/Global.java
@@ -2,10 +2,12 @@ package jelly;
 
 /**
  * Contains global constants
+ * @author Almas
+ * @version 1.0
  */
 public class Global
 {
-  public static final int W = 1280;
+  	public static final int W = 1280;
 	public static final int H = 720;
 	
 	public static final int SPRITE_SIZE = 64;
